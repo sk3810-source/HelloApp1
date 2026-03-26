be93c8eccd7829881d11cf0790cf5b4cb1446b6e
sk3810-source <sk3810@srmist.edu.in>
2026-03-26 10:49:37 +0530

    Update hello app.java

--- hello app.java
+++ hello app.java
@@ -1,1 +1,21 @@
+public class HelloApp {
+    public static void main(String[] args) {
 
+        String result;
+
+        if (args.length == 0) {
+            result = "World";
+        } else {
+            StringBuilder sb = new StringBuilder();
+
+            for (String name : args) {
+                sb.append(name).append(", ");
+            }
+
+            // Remove last ", "
+            result = sb.substring(0, sb.length() - 2);
+        }
+
+        System.out.println("Hello, " + result + "!");
+    }
+}
